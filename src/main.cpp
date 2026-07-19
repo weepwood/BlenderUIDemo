3c72b2f756e5a4fc682c5136bd16b5972dd9559b
weepwood <weepwood@outlook.com>
2026-07-19 12:58:25 +0800

    fix: provide native Windows GUI entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include "App.hpp"
 
+#define SDL_MAIN_HANDLED
 #include <SDL3/SDL_main.h>
 
-int main(int, char**) {
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+namespace {
+
+int SDLCALL run_application(int, char**) {
   blender_ui_demo::App app;
   return app.run();
 }
+
+}  // namespace
+
+#ifdef _WIN32
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+  return SDL_RunApp(0, nullptr, run_application, nullptr);
+}
+
+#else
+
+int main(int argc, char** argv) {
+  return SDL_RunApp(argc, argv, run_application, nullptr);
+}
+
+#endif
